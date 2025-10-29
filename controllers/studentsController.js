265bf2b098f660c100d2d634d375c9d76475b7c5
KharytonchykRaman <157610424+KharytonchykRaman@users.noreply.github.com>
2025-10-29 16:14:06 +0300

    Add files via upload

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -1,32 +1,46 @@
+const path = require("path");
 const formidable = require("formidable");
 
 const FileManager = require("../data/FileManager");
 const DataManager = require("../data/DataManager");
 
 const filterStudent = (req, res) => {
-    let form = new formidable.IncomingForm({
-        keepExtensions: true,
-        uploadDir: UPLOAD_DIR,
-    });
+  let form = new formidable.IncomingForm({
+    keepExtensions: true,
+    uploadDir: FileManager.UPLOAD_DIR,
+  });
 
-    
-}
+  form.on("fileBegin", (name, file) => {
+    if (
+      !FileManager.availableExtnames.some(
+        (ext) => path.extname(file.filepath) === ext
+      )
+    ) {
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          status: "error",
+          message: "not allowed file extname",
+        })
+      );
+    }
 
-const filterGroups = (req, res) => {
+    file.filepath = path.join(FileManager.UPLOAD_DIR, file.originalFilename);
+  });
+};
 
-}
+const filterGroups = (req, res) => {};
 
-const createStudent = (req, res) => {
+const createStudent = (req, res) => {};
 
-}
+const upload = (req, res) => {};
 
-const upload = (req, res) => {
+const changeStatus = (req, res) => {};
 
-}
-
-const changeStatus = (req, res) => {
-
-}
-
-
-module.exports = { filterStudent, filterGroups, createStudent, upload, changeStatus }
+module.exports = {
+  filterStudent,
+  filterGroups,
+  createStudent,
+  upload,
+  changeStatus,
+};
--- data/FileManager.js
+++ data/FileManager.js
@@ -1,18 +1,31 @@
 const fs = require("fs");
 const path = require("path");
+const { upload } = require("../controllers/studentsController");
 
-const STUDENTS_JSON = path.join(__dirname,"students.json");
+const STUDENTS_JSON = path.join(__dirname, "students.json");
+const UPLOAD_DIR = path.join(__dirname, "upload");
+const UPLOAD_DIRS_BY_TYPE = [
+  path.join(UPLOAD_DIR, "text"),
+  path.join(UPLOAD_DIR, "img"),
+];
+
+const availableExtnames = ["txt", "pdf", "docx", "png", "jpg", "jpeg", "webp"];
 
 const getStudents = () => {
-    return JSON.stringify(fs.readFileSync(STUDENTS_JSON));
-}
+  return JSON.stringify(fs.readFileSync(STUDENTS_JSON));
+};
 
 const start = () => {
-    if (!fs.existsSync(STUDENTS_JSON)) {
-        fs.writeFileSync(STUDENTS_JSON,"[]");
+  if (!fs.existsSync(STUDENTS_JSON)) {
+    fs.writeFileSync(STUDENTS_JSON, "[]");
+  }
+  for (const dir of UPLOAD_DIRS_BY_TYPE) {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
     }
-}
+  }
+};
 
 start();
 
-module.exports = {getStudents}
+module.exports = { getStudents, availableExtnames, UPLOAD_DIR };
